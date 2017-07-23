e6972defa72e0efdbcd46881db513e7684dee777
faynco <fayncore@gmail.com>
2017-07-24 04:23:36 +0700

    layouts ready + some header and btn animations

---
+++ assets/js/common.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('.page-header__logo').addClass('animated slideInDown');
+	$('.main-nav__items').addClass('animated slideInDown');
+	$('.home-section__title').addClass('animated slideInLeft');
+	$('.home-section__desc').addClass('animated slideInLeft');
+});
