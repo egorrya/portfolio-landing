f40f40103e980a0073ed945ed4c638f6d4484d0a
faynco <fayncore@gmail.com>
2017-07-25 19:36:37 +0700

    ready for backend

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,6 +1,12 @@
-$(document).ready(function(){
-	$('.page-header__logo').addClass('animated slideInDown');
-	$('.main-nav__items').addClass('animated slideInDown');
-	$('.home-section__title').addClass('animated slideInLeft');
-	$('.home-section__desc').addClass('animated slideInLeft');
+;$(document).ready(function(){
+
+	// animate css
+
+	$(".main-nav__items").animated("fadeInRight");
+	$(".home-section__title").animated("flash");
+	$(".home-section__desc").animated("flash");
+	$(".about__langs").animated("slideInLeft");
+	$(".service__content").animated("fadeInUp");
+	$(".portfolio__items").animated("slideInRight");
+	$(".waiting__10").animated("flash");
 });
