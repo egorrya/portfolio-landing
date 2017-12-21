66c9d4fb647edac7d5d91216aa5fa7f8f7894470
Egor Rya <fayncore@gmail.com>
2017-12-22 05:51:43 +0700

    added the slider and a new portolio item

--- assets/js/common.js
+++ assets/js/common.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
 
         // анимируем переход к блоку, время: 600 мс
         $('body,html').animate({scrollTop: top}, 600);
-      });
+    });
 
 	// animate css
 
@@ -58,4 +58,32 @@ $(document).ready(function(){
 			}
 		});
 	});
-});
+
+// slider
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+
+function nextSlide() {
+	goToSlide(currentSlide+1);
+}
+
+function previousSlide() {
+	goToSlide(currentSlide-1);
+}
+
+function goToSlide(n) {
+	slides[currentSlide].className = 'slide';
+	currentSlide = (n+slides.length)%slides.length;
+	slides[currentSlide].className = 'slide showing';
+}
+
+next.onclick = function() {
+	nextSlide();
+};
+previous.onclick = function() {
+	previousSlide();
+};
+});	
